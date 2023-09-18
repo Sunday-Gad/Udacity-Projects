fbc6452f943eabf2ca1a2ea3399c07ed42926fb6
Sunday Gad <Sundayzephaniahgad@gmail.com>
2023-09-18 13:54:45 +0100

    add second query result

---
+++ Sql project/film2.sql
@@ -0,0 +1,24 @@
+with t1 as (select f.title, c."name", f.rental_duration
+	from film f 
+	join film_category fc 
+	on f.film_id = fc.film_id 
+	join category c 
+	on c.category_id = fc.category_id
+	), 
+	t2 as (select f.title, avg(rental_duration) ave
+		from film f 
+		join film_category fc 
+		on f.film_id = fc.film_id 
+		join category c 
+		on c.category_id = fc.category_id
+		group by 1
+		) 
+		
+		
+select t1.title, t1.name, t1.rental_duration, ntile(4) over (order by t2.ave) standard_quartile
+from t1
+join t2
+on t1.title = t2.title
+where t1."name" in ('Children', 'Music', 'Family', 'Animation', 'Classics', 'Comedy')
+
+
